678575fefaa5c6d5eb70f336af69b4f9d87cf757
Darin <dmcfarlane@fordham.edu>
2024-02-20 20:26:17 -0500

    Adding hw4.js.

---
+++ hw4.js
@@ -0,0 +1,79 @@
+const cats = [
+    {
+        name: 'Charlie',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lily',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Coco',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Oreo',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Luna',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Milo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lola',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Leo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Willow',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Bella',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Max',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Cleo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lucy',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Daisy',
+        adoptionStatus: 'available'
+    },
+]
+
+const Results = [];
+const happy ="Congrats on your new cat:";
+
+for(const cat of cats) {
+    
+if(cat.adoptionStatus==='available')
+     {
+        Results.push(cat.name);
+        console.log(happy,cat.name,"!");
+    
+    }
+}
+
+
+
+
+
+
+
+
